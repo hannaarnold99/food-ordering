df0f4f411b896cbf0406c1be8e4b32bd0a38df97
hannaarnold <hannaarnold99@gmail.com>
2023-10-27 12:24:47 -0700

    changed get

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ const __dirname = path.resolve();
 app.use("/", router);
 
 app.use(express.static(path.join(__dirname, "../build")));
-app.get("/", (req, res) =>
+app.get("/*", (req, res) =>
   res.sendFile(path.join(__dirname, "../build/index.html"))
 );
 
