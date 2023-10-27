0b04deb60d6aa53be5447fbcc66c51489487e175
hannaarnold <hannaarnold99@gmail.com>
2023-10-26 19:44:02 -0700

    changed api request

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to Food Ordering" });
-})
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/build/index.html"))
+);
 
 const PORT = process.env.PORT || 8080; 
 app.listen(PORT, () => {
